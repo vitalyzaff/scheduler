075fd341c0d05c56758e497b1fd1f7f480977b9d
vitalyzaff <vitalyzavgorodnev@gmail.com>
2021-06-07 15:59:48 -0400

    daylist component

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,6 +1,13 @@
 import React from "react";
-
+import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
-  return <ul></ul>
+  const days = props.days.map((day) => {
+    return <DayListItem 
+    name={day.name} 
+    spots={day.spots} 
+    selected={day.name === props.day}
+    setDay={props.setDay}  />
+  })
+  return days;
 }
