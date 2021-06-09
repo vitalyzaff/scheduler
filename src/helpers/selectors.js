bb7b1a94fc31807d7a81033af01addb4f1237ef6
vitalyzaff <vitalyzavgorodnev@gmail.com>
2021-06-09 17:00:38 -0400

    getInterview helper function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -11,3 +11,11 @@ export function getAppointmentsForDay(state, day) {
 }
   return array;
 }
+
+export function getInterview(state, interview) {
+  if (interview) {
+    const obj = { student: interview.student, interviewer: state.interviewers[interview.interviewer]}
+    return obj;
+  } 
+  return null;
+}
